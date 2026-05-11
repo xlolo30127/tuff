51300ae8054c2d027fb472b92f9ff30c6dac48ec
xlolo30127 <lolo30127aaaa@outlook.fr>
2026-05-11 05:34:57 -0700

    Add script_cQxEoAZL_1778502895723.lua

---
+++ script_cQxEoAZL_1778502895723.lua
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=3.0" name="viewport">
+<meta name="description" content="One Hub. Zero Limits. Get Zero Hub now, the best premium Roblox Script Hub. | 2835 members" />
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:site" content="@discord" />
+<meta name="twitter:title" content="Join the Zero Discord Server!" />
+<meta name="twitter:description" content="One Hub. Zero Limits. Get Zero Hub now, the best premium Roblox Script Hub. | 2835 members" />
+<meta property="og:title" content="Join the Zero Discord Server!" />
+<meta property="og:url" content="https://discord.com/invite/zerohub" />
+<meta property="og:description" content="One Hub. Zero Limits. Get Zero Hub now, the best premium Roblox Script Hub. | 2835 members" />
+<meta property="og:site_name" content="Discord" />
+<link rel="canonical" href="https://discord.com/invite/zerohub" />
+<meta property="og:locale:alternate" content="zh-CN" />
+<meta property="og:locale:alternate" content="bg" />
+<meta property="og:locale:alternate" content="cs" />
+<meta property="og:locale:alternate" content="ja" />
+<meta property="og:locale:alternate" content="th" />
+<meta property="og:locale:alternate" content="es-ES" />
+<meta property="og:locale:alternate" content="nl" />
+<meta property="og:locale:alternate" content="fr" />
+<meta property="og:locale:alternate" content="lt" />
+<meta property="og:locale:alternate" content="pl" />
+<meta property="og:locale:alternate" content="en-GB" />
+<meta property="og:locale:alternate" content="fi" />
+<meta property="og:locale:alternate" content="it" />
+<meta property="og:locale:alternate" content="sv-SE" />
+<meta property="og:locale:alternate" content="pt-BR" />
+<meta property="og:locale:alternate" content="ro" />
+<meta property="og:locale:alternate" content="ru" />
+<meta property="og:locale:alternate" content="no" />
+<meta property="og:locale:alternate" content="id" />
+<meta property="og:locale" content="en-US" />
+<meta property="og:locale:alternate" content="el" />
+<meta property="og:locale:alternate" content="he" />
+<meta property="og:locale:alternate" content="ar" />
+<meta property="og:locale:alternate" content="ko" />
+<meta property="og:locale:alternate" content="hi" />
+<meta property="og:locale:alternate" content="uk" />
+<meta property="og:locale:alternate" content="tr" />
+<meta property="og:locale:alternate" content="hu" />
+<meta property="og:locale:alternate" content="es-419" />
+<meta property="og:locale:alternate" content="zh-TW" />
+<meta property="og:locale:alternate" content="de" />
+<meta property="og:locale:alternate" content="da" />
+<meta property="og:locale:alternate" content="vi" />
+<meta property="og:locale:alternate" content="hr" />
+<meta rel="alternate" hreflang="zh-CN" href="https://discord.com/invite/zerohub?locale=zh-CN" />
+<meta rel="alternate" hreflang="bg" href="https://discord.com/invite/zerohub?locale=bg" />
+<meta rel="alternate" hreflang="cs" href="https://discord.com/invite/zerohub?locale=cs" />
+<meta rel="alternate" hreflang="ja" href="https://discord.com/invite/zerohub?locale=ja" />
+<meta rel="alternate" hreflang="th" href="https://discord.com/invite/zerohub?locale=th" />
+<meta rel="alternate" hreflang="es-ES" href="https://discord.com/invite/zerohub?locale=es-ES" />
+<meta rel="alternate" hreflang="nl" href="https://discord.com/invite/zerohub?locale=nl" />
+<meta rel="alternate" hreflang="fr" href="https://discord.com/invite/zerohub?locale=fr" />
+<meta rel="alternate" hreflang="lt" href="https://discord.com/invite/zerohub?locale=lt" />
+<meta rel="alternate" hreflang="pl" href="https://discord.com/invite/zerohub?locale=pl" />
+<meta rel="alternate" hreflang="en-GB" href="https://discord.com/invite/zerohub?locale=en-GB" />
+<meta rel="alternate" hreflang="fi" href="https://discord.com/invite/zerohub?locale=fi" />
+<meta rel="alternate" hreflang="it" href="https://discord.com/invite/zerohub?locale=it" />
+<meta rel="alternate" hreflang="sv-SE" href="https://discord.com/invite/zerohub?locale=sv-SE" />
+<meta rel="alternate" hreflang="pt-BR" href="https://discord.com/invite/zerohub?locale=pt-BR" />
+<meta rel="alternate" hreflang="ro" href="https://discord.com/invite/zerohub?locale=ro" />
+<meta rel="alternate" hreflang="ru" href="https://discord.com/invite/zerohub?locale=ru" />
+<meta rel="alternate" hreflang="no" href="https://discord.com/invite/zerohub?locale=no" />
+<meta rel="alternate" hreflang="id" href="https://discord.com/invite/zerohub?locale=id" />
+<meta rel="alternate" hreflang="en-US" href="https://discord.com/invite/zerohub?locale=en-US" />
+<meta rel="alternate" hreflang="el" href="https://discord.com/invite/zerohub?locale=el" />
+<meta rel="alternate" hreflang="he" href="https://discord.com/invite/zerohub?locale=he" />
+<meta rel="alternate" hreflang="ar" href="https://discord.com/invite/zerohub?locale=ar" />
+<meta rel="alternate" hreflang="ko" href="https://discord.com/invite/zerohub?locale=ko" />
+<meta rel="alternate" hreflang="hi" href="https://discord.com/invite/zerohub?locale=hi" />
+<meta rel="alternate" hreflang="uk" href="https://discord.com/invite/zerohub?locale=uk" />
+<meta rel="alternate" hreflang="tr" href="https://discord.com/invite/zerohub?locale=tr" />
+<meta rel="alternate" hreflang="hu" href="https://discord.com/invite/zerohub?locale=hu" />
+<meta rel="alternate" hreflang="es-419" href="https://discord.com/invite/zerohub?locale=es-419" />
+<meta rel="alternate" hreflang="zh-TW" href="https://discord.com/invite/zerohub?locale=zh-TW" />
+<meta rel="alternate" hreflang="de" href="https://discord.com/invite/zerohub?locale=de" />
+<meta rel="alternate" hreflang="da" href="https://discord.com/invite/zerohub?locale=da" />
+<meta rel="alternate" hreflang="vi" href="https://discord.com/invite/zerohub?locale=vi" />
+<meta rel="alternate" hreflang="hr" href="https://discord.com/invite/zerohub?locale=hr" />
+<meta property="og:image" content="https://cdn.discordapp.com/splashes/1475159237222207489/ad9b974941610cd3101740ef041d0cb6.jpg?size=512" />
+<meta property="og:image:type" content="image/jpeg" />
+<meta property="og:image:width" content="512" />
+<meta property="og:image:height" content="512" />
+<meta property="twitter:image" content="https://cdn.discordapp.com/splashes/1475159237222207489/ad9b974941610cd3101740ef041d0cb6.jpg?size=512" />
+<meta name="googlebot" content="noindex, nofollow" />
+<meta name="bingbot" content="noindex, nofollow" />
+<meta name="slurp" content="noindex, nofollow" />
+<meta name="duckduckbot" content="noindex, nofollow" />
+<meta name="baiduspider" content="noindex, nofollow" />
+<meta name="yandexbot" content="noindex, nofollow" />
+<meta name="naver" content="noindex, nofollow" />
+<meta name="facebookexternalhit" content="index, follow" />
+<meta name="twitterbot" content="index, follow" />
+
+    <!-- plugin:globalConstants -->
+    <!-- plugin:fastConnect -->
+<title>Zero</title>  <script nonce="MTI1LDU0LDE5NSw2LDEyMSwzNSwxODgsMTU3">window.GLOBAL_ENV = {"NODE_ENV":"production","BUILT_AT":"1778261920826","HTML_TIMESTAMP":Date.now(),"BUILD_NUMBER":"541436","PROJECT_ENV":"production","RELEASE_CHANNEL":"stable","VERSION_HASH":"28c300eef21ff861f54a265f19c97fdfa40094f1","PRIMARY_DOMAIN":"discord.com","SENTRY_TAGS":{"buildId":"28c300eef21ff861f54a265f19c97fdfa40094f1","buildType":"normal"},"SENTRY_RELEASE":"2026-05-08-28c300eef21ff861f54a265f19c97fdfa40094f1-discord_web","PUBLIC_PATH":"/assets/","LOCATION":"history","API_VERSION":9,"API_PROTOCOL":"https:","API_ENDPOINT":"//discord.com/api","GATEWAY_ENDPOINT":"wss://gateway.discord.gg","STATIC_ENDPOINT":"","ASSET_ENDPOINT":"//discord.com","MEDIA_PROXY_ENDPOINT":"//media.discordapp.net","IMAGE_PROXY_ENDPOINTS":"//images-ext-1.discordapp.net,//images-ext-2.discordapp.net","CDN_HOST":"cdn.discordapp.com","DEVELOPERS_ENDPOINT":"//discord.com","MARKETING_ENDPOINT":"//discord.com","WEBAPP_ENDPOINT":"//discord.com","WIDGET_ENDPOINT":"//discord.com/widget","SEO_ENDPOINT":"undefined","ADS_MANAGER_ENDPOINT":"//ads.discord.com","NETWORKING_ENDPOINT":"//router.discordapp.net","REMOTE_AUTH_ENDPOINT":"wss://remote-auth-gateway.discord.gg","RTC_LATENCY_ENDPOINT":"//latency.discord.media/rtc","INVITE_HOST":"discord.gg","GUILD_TEMPLATE_HOST":"discord.new","GIFT_CODE_HOST":"discord.gift","ACTIVITY_APPLICATION_HOST":"discordsays.com","MIGRATION_SOURCE_ORIGIN":"https://discordapp.com","MIGRATION_DESTINATION_ORIGIN":"https://discord.com","STRIPE_KEY":"pk_live_CUQtlpQUF0vufWpnpUmQvcdi","ADYEN_KEY":"live_E3OQ33V6GVGTXOVQZEAFQJ6DJIDVG6SY","BRAINTREE_KEY":"production_ktzp8hfp_49pp2rp4phym7387","MUX_ENV_KEY":"undefined","DEV_SESSION_KEY":"undefined","WEBAUTHN_ORIGIN":"discord.com","NATIVE_WEBSOCKET_ORIGIN":"undefined"}</script><script nonce="MTI1LDU0LDE5NSw2LDEyMSwzNSwxODgsMTU3">window.__OVERLAY__ = /overlay/.test(location.pathname);
+window.__BILLING_STANDALONE__ = /^\/billing/.test(location.pathname);</script><link as="script" href="/assets/web-minimal.bbfe1f4f2d05ff05.js" rel="preload"><link as="script" href="/assets/41444.6b72479df7cadc32.js" rel="preload"><link as="script" href="/assets/74926.ada202dcecf667b5.js" rel="preload"><link as="script" href="/assets/8539.c11e602c6d0df356.js" rel="preload"><link as="script" href="/assets/85892.589635a80ea8133a.js" rel="preload"><link as="script" href="/assets/99045.60c8a36fc7ea5fed.js" rel="preload"><link as="script" href="/assets/58858.33aa316f7f9878fc.js" rel="preload"><link as="script" href="/assets/1147.708d8ab127f61417.js" rel="preload"><link as="script" href="/assets/89203.7fc4a877f32d91d0.js" rel="preload"><link as="script" href="/assets/13007.389cd9b86ac723d8.js" rel="preload"><link as="script" href="/assets/25907.df9442a9b6539e5f.js" rel="preload"><link as="script" href="/assets/45187.a5c9ca3e05280cbe.js" rel="preload"><link as="script" href="/assets/75728.5718e2f2f7410b40.js" rel="preload"><link as="script" href="/assets/52706.6412704e90bcff52.js" rel="preload"><link as="script" href="/assets/sentry.f36cc9d429843670.js" rel="preload"><link as="script" href="/assets/fast-connect.618fafee2f18e501.js" rel="preload"><link as="script" href="/assets/libdiscore-wasm-fetch.143c07e1c96db35d.js" rel="preload"><link href="/assets/favicon.ico" rel="icon"><script nonce="MTI1LDU0LDE5NSw2LDEyMSwzNSwxODgsMTU3">!function(){if(null!=window.WebSocket&&function(n){try{var o=localStorage.getItem(n);if(null==o)return null;return JSON.parse(o)}catch(e){return null}}("token")&&!window.__OVERLAY__){var n=null!=window.DiscordNative||null!=window.require?"etf":"json",o=window.GLOBAL_ENV.GATEWAY_ENDPOINT+"/?encoding="+n+"&v="+window.GLOBAL_ENV.API_VERSION;null!=window.DiscordNative&&void 0!==window.Uint8Array&&void 0!==window.TextDecoder?o+="&compress=zstd-stream":void 0!==window.Uint8Array&&(o+="&compress=zlib-stream"),console.log("[FAST CONNECT] "+o+", encoding: "+n+", version: "+window.GLOBAL_ENV.API_VERSION);var e=new WebSocket(o);e.binaryType="arraybuffer";var i=Date.now(),r={open:!1,identify:!1,gateway:o,messages:[]};e.onopen=function(){console.log("[FAST CONNECT] connected in "+(Date.now()-i)+"ms"),r.open=!0},e.onclose=e.onerror=function(){window._ws=null},e.onmessage=function(n){r.messages.push(n)},window._ws={ws:e,state:r}}}();</script><script defer src="/assets/libdiscore-wasm-fetch.143c07e1c96db35d.js"></script><script defer src="/assets/fast-connect.618fafee2f18e501.js"></script><script defer src="/assets/sentry.f36cc9d429843670.js"></script><script defer src="/assets/52706.6412704e90bcff52.js"></script><script defer src="/assets/75728.5718e2f2f7410b40.js"></script><script defer src="/assets/45187.a5c9ca3e05280cbe.js"></script><script defer src="/assets/25907.df9442a9b6539e5f.js"></script><script defer src="/assets/13007.389cd9b86ac723d8.js"></script><script defer src="/assets/89203.7fc4a877f32d91d0.js"></script><script defer src="/assets/1147.708d8ab127f61417.js"></script><script defer src="/assets/58858.33aa316f7f9878fc.js"></script><script defer src="/assets/99045.60c8a36fc7ea5fed.js"></script><script defer src="/assets/85892.589635a80ea8133a.js"></script><script defer src="/assets/8539.c11e602c6d0df356.js"></script><script defer src="/assets/74926.ada202dcecf667b5.js"></script><script defer src="/assets/41444.6b72479df7cadc32.js"></script><script defer src="/assets/web-minimal.bbfe1f4f2d05ff05.js"></script><link href="/assets/52706.0eaa18cf2ce282f2.css" rel="stylesheet"><link href="/assets/75728.7e71edf02e645d32.css" rel="stylesheet"><link href="/assets/45187.caf6ca089016de09.css" rel="stylesheet"><link href="/assets/25907.a08c9fe1c38e041a.css" rel="stylesheet"><link href="/assets/13007.6cbefcd6949684c4.css" rel="stylesheet"><link href="/assets/89203.d5502022dd30df08.css" rel="stylesheet"><link href="/assets/1147.c2916c42d5ddc9f9.css" rel="stylesheet"><link href="/assets/58858.f7ea0a6c4421fa39.css" rel="stylesheet"><link href="/assets/99045.4dcf9c9ce28d3f2c.css" rel="stylesheet"><link href="/assets/85892.08f51a4958cbd9d6.css" rel="stylesheet"><link href="/assets/8539.316be4cdf7ee32a5.css" rel="stylesheet"><link href="/assets/74926.01596b55d28e176e.css" rel="stylesheet"><link href="/assets/41444.5ea888d7f819b0cf.css" rel="stylesheet"><link href="/assets/76342.68853a1e0b9d94df.css" rel="stylesheet"><link href="/assets/56886.64222f84fb414777.css" rel="stylesheet"></head>
+
+  <body>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="app-mount"></div>
+  
+
+<script nonce="MTI1LDU0LDE5NSw2LDEyMSwzNSwxODgsMTU3">(function(){function c(){var b=a.contentDocument||a.contentWindow.document;if(b){var d=b.createElement('script');d.nonce='MTI1LDU0LDE5NSw2LDEyMSwzNSwxODgsMTU3';d.innerHTML="window.__CF$cv$params={r:'9fa138153fc93cd5',t:'MTc3ODUwMjg3OQ=='};var a=document.createElement('script');a.nonce='MTI1LDU0LDE5NSw2LDEyMSwzNSwxODgsMTU3';a.src='/cdn-cgi/challenge-platform/scripts/jsd/main.js';document.getElementsByTagName('head')[0].appendChild(a);";b.getElementsByTagName('head')[0].appendChild(d)}}if(document.body){var a=document.createElement('iframe');a.height=1;a.width=1;a.style.position='absolute';a.style.top=0;a.style.left=0;a.style.border='none';a.style.visibility='hidden';document.body.appendChild(a);if('loading'!==document.readyState)c();else if(window.addEventListener)document.addEventListener('DOMContentLoaded',c);else{var e=document.onreadystatechange||function(){};document.onreadystatechange=function(b){e(b);'loading'!==document.readyState&&(document.onreadystatechange=e,c())}}}})();</script></body></html>

